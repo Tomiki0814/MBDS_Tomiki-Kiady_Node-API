fee027ff861f58d26fffd5d07a35c7140c467b5a
Tomiki0814 <tomikiramanantsoa@gmail.com>
2023-06-15 11:45:47 +0300

    hide password in login

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,13 @@ let user = require('../model/user');
 function Login(req, res) {
     let name = req.body.username;
     let pwd = req.body.mdp
-    user.findOne({nom: name, password: pwd}, (err, assignment) => {
+    user.findOne({nom: name, password: pwd}, (err, result) => {
         if (err) {
             res.send(err)
         }
-        res.json(assignment);
+        let userResult = new user(result)
+        userResult.password = null;
+        res.json(userResult);
     })
 }
 
