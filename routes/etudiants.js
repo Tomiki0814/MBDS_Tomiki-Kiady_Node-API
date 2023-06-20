279ac6d087beeac5c15f1f91147203f91e13b679
Tomiki0814 <tomikiramanantsoa@gmail.com>
2023-06-20 21:00:11 +0300

    nettoyer code

--- routes/etudiants.js
+++ routes/etudiants.js
@@ -4,61 +4,13 @@ let matiere = require('../model/matiere')
 
 function getEtudiantsSansPagination(req, res) {
     var listeEtudiant;
-    var listeMatiere;
     etudiant.find((err, assignments) => {
         if (err) {
             res.send(err)
         }
         listeEtudiant = assignments;
-        //console.log(listeEtudiant)
         res.send(assignments);
-        /*matiere.find((err, assignments) => {
-            if(err){
-                res.send(err)
-            }
-            listeMatiere = assignments;
-            var listeResult = [];
-            var stringjson="";
-            let listDate=['2023-06-17','2023-06-18','2023-06-19','2023-06-20','2023-06-21'];
-            let listcommentairebon = ['Assez bien','Bon travail','Peut encore mieux faire','Bon resultat','Bien jouer'];
-            let listcommentairemauvais = ['Peut mieux faire','Encore un effort','Travailler plus',"Faite plus d'effort",'Travail mal faite'];
-            for(var i=0; i<listeEtudiant.length; i++){
-                for(var a=0; a<listeMatiere.length; a++){
-                    var note = Math.floor( Math.random() * (17.5 - 6)) + 6
-                    var commentNumber = Math.floor(Math.random() * 5);
-                    var dateNumber = Math.floor(Math.random() * 5);
-                    if(note>13){
-                        stringjson+= '{"idEtudiant" : {"$oid": "'+listeEtudiant[i]._id+'"}, '
-                        stringjson+= '"idMatiere": {"$oid": "'+listeMatiere[a]._id+'"},'
-                        stringjson+= '"dateRendu":'+'"'+listDate[dateNumber]+'" ,'
-                        if(i%2==0){
-                            stringjson+='"estRendu": true ,'
-                            stringjson+= '"note": '+note+','
-                            stringjson+= '"remarque": "'+listcommentairebon[commentNumber]+'"},\n'
-                        }else{
-                            stringjson+='"estRendu": false'
-                            stringjson+='},\n'
-                        }
 
-                    }else{
-                        stringjson+= '{"idEtudiant" : {"$oid": "'+listeEtudiant[i]._id+'"}, '
-                        stringjson+= '"idMatiere": {"$oid": "'+listeMatiere[a]._id+'"},'
-                        stringjson+= '"dateRendu":'+'"'+listDate[dateNumber]+'" ,'
-                        if(i%2==0){
-                            stringjson+='"estRendu": true ,'
-                            stringjson+= '"note": '+note+','
-                            stringjson+= '"remarque": "'+listcommentairemauvais[commentNumber]+'"},\n'
-                        }else{
-                            stringjson+='"estRendu": false'
-                            stringjson+='},\n'
-                        }
-                    }
-
-                }
-
-            }
-            res.send(stringjson)
-        });*/
     });
 
 
